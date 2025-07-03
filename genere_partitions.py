c94ec4eb5b0b3f3925a55f67bfd158746de75ed8
maridasi47500 <61146153+maridasi47500@users.noreply.github.com>
2025-07-03 01:14:31 -0300

    Add files via upload

---
+++ genere_partitions.py
@@ -0,0 +1,29 @@
+import yaml
+
+def genere_partitions(fichier_yaml):
+    with open(fichier_yaml, "r", encoding="utf-8") as f:
+        data = yaml.safe_load(f)
+
+    for exercice in data["exercices"]:
+        titre = exercice["titre"]
+        annotation = exercice.get("annotation", "")
+        notes = " ".join(exercice["notes"])
+
+        ly_code = f'''
+\\markup {{ \\bold "{titre}" }}
+\\markup {{ "{annotation}" }}
+
+\\score {{
+  \\new Staff {{
+    \\clef treble
+    \\time 4/4
+    \\relative c' {{
+      {notes}
+    }}
+  }}
+}}
+'''
+        with open(f"{titre.replace(' ', '_')}.ly", "w", encoding="utf-8") as out:
+            out.write(ly_code)
+
+genere_partitions('./exercise.yaml')
